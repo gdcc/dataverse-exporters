bbb3439b91f245cbc8cbf8b91efc875a9083141d
Philip Durbin <philip_durbin@harvard.edu>
2024-05-01 16:21:47 -0400

    add min and max to valiate script

--- croissant/validate.sh
+++ croissant/validate.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+mlcroissant validate --jsonld src/test/resources/minimal/croissant.json
+mlcroissant validate --jsonld src/test/resources/max/croissant.json
 mlcroissant validate --jsonld src/test/resources/cars/croissant.json
